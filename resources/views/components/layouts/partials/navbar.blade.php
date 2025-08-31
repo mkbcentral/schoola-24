f70ac2d2bbe1c6be59b2d37333384f48aaa5b15e
mkbcentral <mkbcentralcode@gmail.com>
2025-08-31 17:37:27 +0200

    Work 31.08.2025

--- resources/views/components/layouts/partials/navbar.blade.php
+++ resources/views/components/layouts/partials/navbar.blade.php
@@ -3,7 +3,11 @@
         <button type="button" id="sidebarCollapse" class="btn btn-info">
             <i class="bi bi-list"></i>
         </button>
-        <livewire:application.widgets.school-year-label />
+        <!-- Show only on desktop -->
+        <div class="d-none d-lg-block">
+            <livewire:application.widgets.school-year-label />
+        </div>
+        <!-- Desktop User Dropdown -->
         <div class="d-none d-lg-flex align-items-center">
             <div class="dropdown ms-3">
                 <button class="btn dropdown-toggle d-flex align-items-center" type="button" id="userDropdown"
@@ -22,7 +26,34 @@
                             @csrf
                             <a href="#"
                                 onclick="event.preventDefault();
-                        this.closest('form').submit();"
+                this.closest('form').submit();"
+                                class="dropdown-item">
+                                <i class="bi bi-box-arrow-right me-2"></i>Se déconnecter</a>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- Mobile User Dropdown -->
+        <div class="d-flex d-lg-none align-items-center ms-auto">
+            <div class="dropdown">
+                <button class="btn dropdown-toggle d-flex align-items-center" type="button" id="userDropdownMobile"
+                    data-bs-toggle="dropdown">
+                    <i class="bi bi-person-circle me-2"></i>
+
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li><a href="{{ route('admin.user.profile') }}" class="dropdown-item">Profile</a></li>
+                    <li><a class="dropdown-item" href="{{ route('settings.main') }}">Settings</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <a href="#"
+                                onclick="event.preventDefault();
+                this.closest('form').submit();"
                                 class="dropdown-item">
                                 <i class="bi bi-box-arrow-right me-2"></i>Se déconnecter</a>
                         </form>
--- resources/views/livewire/application/widgets/school-year-label.blade.php
+++ resources/views/livewire/application/widgets/school-year-label.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <div class="d-flex flex-wrap align-items-center shadow-sm rounded-3 p-2 gap-2
-       " style="max-width: 180px;">
-        <div class="flex-grow-1" style="min-width: 170px;">
-            <h5 class="text-center text-primary">{{ $schoolYear->name }}</h5>
+    <div class="d-flex flex-column align-items-center shadow-sm rounded-3 p-2 gap-2"
+        style="max-width: 100%; min-width: 0;">
+        <div class="w-100">
+            <h5 class="text-center text-primary" style="font-size: 1.1rem;">{{ $schoolYear->name }}</h5>
         </div>
     </div>
 </div>
